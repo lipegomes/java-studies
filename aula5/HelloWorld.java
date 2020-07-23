f98038a52c57245590449ba2fc0d2aa11549d577
filipe <fgdl.py91@gmail.com>
2020-07-22 21:16:12 -0300

    corrected comment

--- aula5/HelloWorld.java
+++ aula5/HelloWorld.java
@@ -1,8 +1,8 @@
 // Aula 5 - Primeiro programa em java
 
 /**
- * O nome da classe deve começar com letra maiúscula(CamelCase) O arquivo deve
- * ser salvo com o nome da classe.
+ * O nome da classe deve começar com letra maiúscula(CamelCase)
+ * O arquivo deve ser salvo com o nome da classe.
  */
 
 public class HelloWorld {
