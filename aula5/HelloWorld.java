a0010525b91449285465f4b4581b8eca8674dc3a
filipe <fgdl.py91@gmail.com>
2020-07-22 21:11:22 -0300

    fisrt commit

---
+++ aula5/HelloWorld.java
@@ -0,0 +1,12 @@
+// Aula 5 - Primeiro programa em java
+
+/**
+ * O nome da classe deve começar com letra maiúscula(CamelCase) O arquivo deve
+ * ser salvo com o nome da classe.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+    };
+}
