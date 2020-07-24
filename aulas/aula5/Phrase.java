b0f02c2d5be9a317c7f6bf28c7cc8ac5656e0baa
filipe <fgdl.py91@gmail.com>
2020-07-24 20:57:30 -0300

    new commit

---
+++ aulas/aula5/Phrase.java
@@ -0,0 +1,8 @@
+public class Phrase {
+    public static void main(String[] args) {
+        System.out.println("Make your dreams come true.");
+    }
+}
+
+// Compilar o programa: $ javac <Nome da classe>.java
+// Executar a classe após compilada: $ java <Nome da classe>
