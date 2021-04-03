6128972244e02642f19753656e748625d4465916
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-03 11:44:15 -0300

    Update operators

--- src/academy/devdojo/maratonajava/introducao/Lesson03PrimitiveTypesExercises.java
+++ src/academy/devdojo/maratonajava/introducao/Lesson03PrimitiveTypesExercises.java
@@ -11,7 +11,7 @@ confirmo que recebi o salário de <salario>, na data <data>
 public class Lesson03PrimitiveTypesExercises {
     public static void main(String[] args) {
         String name = "Josh";
-        String address = "Lincoln Avenue number 1090 apartment 23";
+        String address = "1090 Lincoln Avenue, 23 apartment";
         double salary = 12754.21;
         String receivingDate = "03/10/2021";
         String message = "I " + name + " living at the address " + address + ", I confirm that I received the salary of " + "$" + salary + " on date " + receivingDate + ".";
