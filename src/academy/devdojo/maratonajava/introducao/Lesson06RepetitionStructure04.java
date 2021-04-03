2c413162dc94e7e95ac35d6213a12ed61f255fba
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-03 18:11:11 -0300

    Repetition structure added

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson06RepetitionStructure04.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson06RepetitionStructure04 {
+    // Given the value of a car, find out how many times it can be paid in installments.
+    // Condition installmentValue >= 1000
+    public static void main(String[] args) {
+        double totalValue = 38000;
+        for (int installment = 1; installment <= totalValue; installment++) {
+            double installmentValue = totalValue / installment;
+            if (installmentValue < 1000) {
+                break;
+            }
+            System.out.println("Installment " + installment + ": $" + installmentValue);
+        }
+    }
+}
