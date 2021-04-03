7aa251dfb27f852ced7152cb6e2b100ef3735a5d
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-03 14:53:42 -0300

    Conditional structures added

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson05ConditionalStructures02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson05ConditionalStructures02 {
+    public static void main(String[] args) {
+        // age < 15 infant category
+        // age >= 15 && age < 18 youthful category
+        // age >= 18 adult category
+        int age = 30;
+        String category;
+
+        if (age < 15) {
+            category = "Infant Category";
+        } else if (age >= 15 && age < 18) {
+            category = "Youthful Category";
+        } else {
+            category = "Adult Category";
+        }
+        System.out.println(category);
+    }
+}
