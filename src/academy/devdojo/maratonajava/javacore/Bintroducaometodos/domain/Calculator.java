48b679e42d4b75549ec7fcebe4550046f887bf15
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-04 16:10:20 -0300

    Methods and classes added

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculator.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Calculator {
+    public void sumNumbers() {
+        System.out.println(2 + 2);
+    }
+
+    public void subNumbers() {
+        System.out.println(10 - 3);
+    }
+
+    public void multiNumbers() {
+        System.out.println(7 * 7);
+    }
+
+    public void divNumbers() {
+        System.out.println(100 / 5);
+    }
+}
