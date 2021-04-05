5c9135253aa9a708a34aadb8cf289f3f46c4ace8
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-05 18:30:54 -0300

    Methods varargs and exercises

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/Calculator05Test01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Calculator05;
+
+public class Calculator05Test01 {
+    public static void main(String[] args) {
+        Calculator05 calculator = new Calculator05();
+        int[] numbers = {1, 2, 3, 4, 5};
+        calculator.sumArray(numbers);
+        calculator.sumVarArgs(2, 4, 6);
+    }
+}
