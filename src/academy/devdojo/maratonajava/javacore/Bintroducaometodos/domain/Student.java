8cb741ff4e5a01b63a2a753d659a17190b753c55
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-05 12:48:40 -0300

    Reference this type parameters added

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Student.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Student.java
@@ -5,4 +5,12 @@ public class Student {
     public int age;
     public char gender;
     public String music;
+
+    public void prints(){
+        System.out.println("--------");
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: "+ this.age);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Music: " + this.music);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/StudentTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Student;
+
+public class StudentTest02 {
+    public static void main(String[] args) {
+        Student student01 = new Student();
+        Student student02 = new Student();
+
+        student01.name = "Petter";
+        student01.age = 60;
+        student01.gender = 'M';
+        student01.music = "Classic";
+
+        student02.name = "Jenny";
+        student02.age = 42;
+        student02.gender = 'F';
+        student02.music = "Caribbean";
+
+        student01.prints();
+        student02.prints();
+    }
+}
