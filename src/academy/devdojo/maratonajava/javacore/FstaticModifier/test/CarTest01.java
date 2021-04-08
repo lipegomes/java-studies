1ee92b2dbb9518daf04e9da0a4ae4f2bd119e042
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-08 11:39:05 -0300

    Static method added

--- src/academy/devdojo/maratonajava/javacore/FstaticModifier/test/CarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/FstaticModifier/test/CarTest01.java
@@ -4,11 +4,10 @@ import academy.devdojo.maratonajava.javacore.FstaticModifier.domain.Car;
 
 public class CarTest01 {
     public static void main(String[] args) {
+        Car.setLimitSpeed(220);
         Car car01 = new Car("Mercedes-Benz", 298);
         Car car02 = new Car("Ferrari", 310);
         Car car03 = new Car("Porsche", 335);
-
-        Car.limitSpeed = 180;
 
         car01.prints();
         car02.prints();
