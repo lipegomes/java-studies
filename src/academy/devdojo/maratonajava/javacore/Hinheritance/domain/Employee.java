e7943af0f089bcfea0bdb5aed851a59fc2c1559a
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-19 16:43:52 -0300

    Inheritance constructors added

--- src/academy/devdojo/maratonajava/javacore/Hinheritance/domain/Employee.java
+++ src/academy/devdojo/maratonajava/javacore/Hinheritance/domain/Employee.java
@@ -3,13 +3,17 @@ package academy.devdojo.maratonajava.javacore.Hinheritance.domain;
 public class Employee extends Person {
     private double salary;
 
+    public Employee(String name) {
+        super(name);
+    }
+
     public void prints() {
         super.prints();
         System.out.println("Salary: $" + this.salary);
     }
 
     public void paymentReport() {
-        System.out.println("I " + this.name + ", received the salary in the amount of " + this.salary);
+        System.out.println("Me " + this.name + ", received the salary in the amount of " + this.salary);
     }
 
     public double getSalary() {
