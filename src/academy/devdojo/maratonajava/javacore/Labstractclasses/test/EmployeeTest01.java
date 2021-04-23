1e2c416a3ac6d1f243dd1a3c5ddcf3416d185588
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-23 11:15:04 -0300

    Abstract methods added

--- src/academy/devdojo/maratonajava/javacore/Labstractclasses/test/EmployeeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Labstractclasses/test/EmployeeTest01.java
@@ -6,8 +6,12 @@ import academy.devdojo.maratonajava.javacore.Labstractclasses.domain.SoftwareEng
 public class EmployeeTest01 {
     public static void main(String[] args) {
         Manager manager = new Manager("John", 26000);
-        SoftwareEngineer softwareEngineer = new SoftwareEngineer("Scott", 12000);
+        SoftwareEngineer softwareEngineer1 = new SoftwareEngineer("Scott", 12000);
+        SoftwareEngineer softwareEngineer2 = new SoftwareEngineer("Petter", 9000);
+        SoftwareEngineer softwareEngineer3 = new SoftwareEngineer("Gracie", 12000);
         System.out.println(manager);
-        System.out.println(softwareEngineer);
+        System.out.println(softwareEngineer1);
+        System.out.println(softwareEngineer2);
+        System.out.println(softwareEngineer3);
     }
 }
