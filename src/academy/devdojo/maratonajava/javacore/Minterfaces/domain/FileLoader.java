02934ce7900acf804c4220725966dc432cb1b693
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-26 11:28:23 -0300

    Implementing multiple interfaces added

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/FileLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/FileLoader.java
@@ -1,8 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.domain;
 
-public class FileLoader implements DataLoader {
+public class FileLoader implements DataLoader, DataRemove {
     @Override
     public void load() {
         System.out.println("Loading data from a file....");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removing data from the file....");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checking permission in the file...");
     }
 }
