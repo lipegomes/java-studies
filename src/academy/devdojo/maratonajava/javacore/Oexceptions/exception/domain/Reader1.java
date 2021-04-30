ee07bac7164b74daf122f8585f47468168c85cf7
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-04-30 13:41:41 -0300

    Exceptions - try with resources added

---
+++ src/academy/devdojo/maratonajava/javacore/Oexceptions/exception/domain/Reader1.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Oexceptions.exception.domain;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Reader1 implements Closeable {
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Closing the reader 1");
+    }
+}
