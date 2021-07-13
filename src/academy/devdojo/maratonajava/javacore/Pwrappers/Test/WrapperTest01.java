db9178d06f29298ae5ced4fdc4e99022d3808d24
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-07-13 19:52:36 -0300

    Wrapper Classes - part 2

--- src/academy/devdojo/maratonajava/javacore/Pwrappers/Test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrappers/Test/WrapperTest01.java
@@ -9,17 +9,34 @@ public class WrapperTest01 {
         long longP = 10L;
         float floatP = 10F;
         double doubleP = 10D;
-        char charP = 'W';
+        char charP = 'F';
         boolean booleanP = false;
 
         // Wrapper - Classes representing primitive types
+        // Autoboxing
         Byte byteW = 1;
         Short shortW = 1;
         Integer intW = 1;
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
-        Character charW = 'W';
+        Character charW = 'F';
         Boolean booleanW = false;
+
+        // Unboxing
+        int i = intW;
+        Integer intW2 = Integer.parseInt("10");
+        Double intW3 = Double.parseDouble("25.4");
+        boolean brazil = Boolean.parseBoolean("TruE");
+
+        System.out.println(brazil);
+        System.out.println(Character.isDigit('B'));
+        System.out.println(Character.isDigit('8'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('d'));
+        System.out.println(Character.isLetter('5'));
+        System.out.println(Character.toUpperCase('e'));
+        System.out.println(Character.toLowerCase('T'));
     }
 }
