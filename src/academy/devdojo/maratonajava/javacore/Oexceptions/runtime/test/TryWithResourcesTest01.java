042db066b15bc6712b3ff21b5ae243e8411f1551
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-07-13 16:11:34 -0300

    Removed unnecessary semicolon

--- src/academy/devdojo/maratonajava/javacore/Oexceptions/runtime/test/TryWithResourcesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexceptions/runtime/test/TryWithResourcesTest01.java
@@ -12,7 +12,7 @@ public class TryWithResourcesTest01 {
 
     public static void readFile() {
         try (Reader1 reader1 = new Reader1();
-             Reader2 reader2 = new Reader2();) {
+             Reader2 reader2 = new Reader2()) {
 
         } catch (IOException e) {
             e.printStackTrace();
