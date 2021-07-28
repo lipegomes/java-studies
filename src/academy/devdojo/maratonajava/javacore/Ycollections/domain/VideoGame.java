0be67ba6951623ca0638178b964c8e2040c7bd4f
lipegomes <fgdl.py91@gmail.com>
2021-07-28 11:47:45 -0300

    Collections pt 02 - equals - override equals

---
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/domain/VideoGame.java
@@ -0,0 +1,72 @@
+package academy.devdojo.maratonajava.javacore.Ycollections.domain;
+
+public class VideoGame {
+    private String brand;
+    private String model;
+    private String color;
+    private String ItemWeight;
+    private String ProductDimensions;
+
+    // Método construtor
+    public VideoGame(String brand, String model, String color, String itemWeight, String productDimensions) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.ItemWeight = itemWeight;
+        this.ProductDimensions = productDimensions;
+    }
+
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente  de null
+    // Para x diferente de null, x.equals(null) tem que retornar false.
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return  true;
+        if (this.getClass() != obj.getClass()) return false;
+        VideoGame videoGame = (VideoGame) obj;
+        return brand != null && brand.equals(videoGame.brand);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getItemWeight() {
+        return ItemWeight;
+    }
+
+    public void setItemWeight(String itemWeight) {
+        this.ItemWeight = itemWeight;
+    }
+
+    public String getProductDimensions() {
+        return ProductDimensions;
+    }
+
+    public void setProductDimensions(String productDimensions) {
+        this.ProductDimensions = productDimensions;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/TestVideoGame01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Ycollections.test;
+
+import academy.devdojo.maratonajava.javacore.Ycollections.domain.VideoGame;
+
+public class TestVideoGame01 {
+    public static void main(String[] args) {
+        VideoGame xboxSeriesS = new VideoGame(
+                "Microsoft",
+                "Xbox Series S",
+                "White",
+                "4.23 pounds",
+                " 5.94 x 2.56 x 10.83 inches"
+        );
+        VideoGame playStation5 = new VideoGame(
+                "Sony",
+                "Playstation 5",
+                "White",
+                "14.2 pounds",
+                " 18 x 18 x 12 inches"
+        );
+        System.out.println("xboxSeriesS = playStation5 ?  " + xboxSeriesS.equals(playStation5));
+    }
+}
