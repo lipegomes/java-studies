142d6d91ce6dd4946524dd4f6124974eef2bca0f
filipe gomes <fgdl.py91@gmail.com>
2021-07-30 14:09:11 -0300

    Collections pt 06 - List pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/TestList01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Ycollections.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestList01 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(10);
+        names.add("Sergei");
+        names.add("Rachmaninoff");
+
+        for (String  name: names) {
+            System.out.println(name);
+        }
+        System.out.println("-----");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
+}
