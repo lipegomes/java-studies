dcbea5fa9bfdcf07a705b4c43d417f9537ba2440
filipe gomes <fgdl.py91@gmail.com>
2021-08-02 16:39:54 -0300

    Collections pt 17 - NavigableSet, TreeSet pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycollections/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/NavigableSetTest01.java
@@ -14,18 +14,43 @@ class SmartphoneBrandComparator implements Comparator<Smartphone> {
         return smartphone1.getBrand().compareTo(smartphone2.getBrand());
     }
 }
+
+class FilmRateComparator implements Comparator<Film> {
+
+    @Override
+    public int compare(Film film1, Film film2) {
+        return Float.compare(film1.getRate(), film2.getRate());
+    }
+}
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneBrandComparator());
         Smartphone smartphone = new Smartphone("AZ87QE5RQ1","Xiaomi");
         set.add(smartphone);
 
-        NavigableSet<Film> films = new TreeSet<>();
-        films.add(new Film(2L, "Saving Private Ryan", 8.6f, 1998, "Drama, War", 169));
-        films.add(new Film(1L, "The Pianist", 8.5f, 2002, "Biography, Drama, Music", 150));
+        NavigableSet<Film> films = new TreeSet<>(new FilmRateComparator());
+        films.add(new Film(5L, "Big Hero 6", 7.8f, 2014, "Animation, Action, Adventure", 102,  424_555L, 222.53f));
+        films.add(new Film(2L, "Spirited Away", 8.6f, 2001, "Animation, Adventure, Family", 125,  682_506L, 10.06f));
+        films.add(new Film(3L, "Toy Story 3", 8.2f, 2010, "Animation, Adventure, Comedy", 103,  778_139L, 415.0f));
+        films.add(new Film(1L, "Your Name", 8.4f, 2016, "Animation, Drama, Fantasy", 106,  217_234L, 5.02f));
 
+        System.out.println("--- Sem ordenação ---");
         for (Film film : films) {
             System.out.println(film);
         }
+
+        Film zootopia = new Film(4L, "Zootopia", 8f, 2016, "Animation, Adventure, Comedy", 108,  452_150L, 341.27f);
+
+        System.out.println("\n--- Ordenado por lower ---");
+        System.out.println(films.lower(zootopia));
+
+        System.out.println("\n--- Ordenado por floor ---");
+        System.out.println(films.floor(zootopia));
+
+        System.out.println("\n--- Ordenado por higher ---");
+        System.out.println(films.higher(zootopia));
+
+        System.out.println("\n--- Ordenado por ceiling ---");
+        System.out.println(films.ceiling(zootopia));
     }
 }
