4dccdd8753144ebf419f504fb418298b5a443f07
filipe gomes <fgdl.py91@gmail.com>
2021-09-11 22:09:54 -0300

    Refatorado código e adicionado novos métodos

--- java-object-oriented/object-oriented/src/gomes/filipe/exceptions/TestExceptions.java
+++ java-object-oriented/object-oriented/src/gomes/filipe/exceptions/TestExceptions.java
@@ -4,31 +4,67 @@ public class TestExceptions {
 
     public static void main(String[] args) {
         tabuada();
-        divisao(10, 0);
+        soma(20, 35);
+        subtracao(44, Math.sqrt(1444));
+        divisao(5, 0);
+        multiplicacao(7, 7);
     }
 
     private static void tabuada() {
-        System.out.println("\n--- inicio da tabuada ---");
+        System.out.println("--- inicio da tabuada ---");
         for (int i = 0 ; i <= 10; i++) {
             try {
-                for (int j = 0 ; j <= 10; j++) {
+                for (int j = 1 ; j <= 10; j++) {
                     int calc = j * i ;
-                    System.out.println(calc);
+                    System.out.println(i + " * " + j + " = " + calc);
                 }
             } catch (ArithmeticException e) {
                 e.printStackTrace();
             }
             System.out.println();
         }
-        System.out.println("\n--- Fim da tabuada ---");
+        System.out.println("--- Fim da tabuada ---\n");
+    }
+
+    private static void soma(double x, double y) {
+        try {
+            System.out.println("--- inicio da soma ---");
+            double resultado = x + y;
+            System.out.println(resultado);
+            System.out.println("--- fim da soma ---\n");
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void subtracao(double x, double y) {
+        try {
+            System.out.println("--- inicio da subtracao ---");
+            double resultado = x - y;
+            System.out.println(resultado);
+            System.out.println("--- fim da subtracao ---\n");
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void divisao(int x, int y) {
         try {
-            System.out.println("\n--- inicio da divisao ---");
+            System.out.println("--- inicio da divisao ---");
             int resultado = x / y;
             System.out.println(resultado);
-            System.out.println("\n--- fim da divisao ---");
+            System.out.println("--- fim da divisao ---\n");
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void multiplicacao(double x, double y) {
+        try {
+            System.out.println("--- inicio da multiplicacao ---");
+            double resultado = x * y;
+            System.out.println(resultado);
+            System.out.println("--- fim da multiplicacao ---\n");
         } catch (ArithmeticException e) {
             e.printStackTrace();
         }
