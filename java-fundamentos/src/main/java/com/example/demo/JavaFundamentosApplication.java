f39f0c1aa7de52bed8e0798d7b42a25636ad1ae6
Filipe Gomes <fgdl.py91@gmail.com>
2021-12-05 09:24:03 -0300

    Adicionado pasta java fundamentos

---
+++ java-fundamentos/src/main/java/com/example/demo/JavaFundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFundamentosApplication.class, args);
+	}
+
+}
