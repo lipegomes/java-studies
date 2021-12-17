fbf7726b712e4069211be19ada83813cf461b4e1
Filipe Gomes <fgdl.py91@gmail.com>
2021-12-17 19:34:28 -0300

    Modificado a classe RadianToDegree

--- java-object-oriented/object-oriented/src/gomes/filipe/mathematics/domain/RadianToDegree.java
+++ java-object-oriented/object-oriented/src/gomes/filipe/mathematics/domain/RadianToDegree.java
@@ -6,7 +6,7 @@ public class RadianToDegree {
         double PI_RADIANS = 180.0;
         double ONE_RADIANS = PI_RADIANS / Math.PI;
         double degrees = radiance * ONE_RADIANS;
-
+        
         System.out.println("Radiance " + radiance + " in degrees: " + degrees);
     }
 }
