71430ab2862ef35e26804120d10ab9ec4db44da1
Filipe Gomes <fgdl.py91@gmail.com>
2021-12-18 14:09:42 -0300

    01 A alternativa threadsafe

---
+++ java-object-oriented/object-oriented/src/gomes/filipe/polymorphism/mybank/test/TestLinkedList.java
@@ -0,0 +1,30 @@
+package gomes.filipe.polymorphism.mybank.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gomes.filipe.polymorphism.mybank.model.Conta;
+import gomes.filipe.polymorphism.mybank.model.ContaCorrente;
+
+public class TestLinkedList {
+
+	public static void main(String[] args) {
+
+		//Linked List - thread safe
+		List<Conta> lista = new ArrayList<Conta>();
+
+		Conta cc1 = new ContaCorrente("Banco A", 1, 123);
+		lista.add(cc1);
+
+		Conta cc2 = new ContaCorrente("Banco A", 1, 456);
+		lista.add(cc2);
+
+		Conta cc3 = new ContaCorrente("Banco A", 1, 987);
+		boolean contaExistente = lista.contains(cc3);
+		System.out.println("Conta existente: " + contaExistente);
+
+		for(Conta conta : lista ) {
+			System.out.println(conta);
+		}
+	}
+}
