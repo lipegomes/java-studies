a51ea6b8fc0bde8b866c423ea4a68181aa27b403
Filipe Gomes <fgdl.py91@gmail.com>
2021-12-19 11:24:51 -0300

    02 Métodos da classe Integer

---
+++ java-object-oriented/object-oriented/src/gomes/filipe/polymorphism/mybank/test/TestClasseInteger.java
@@ -0,0 +1,37 @@
+package gomes.filipe.polymorphism.mybank.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestClasseInteger {
+	
+	public static void main(String[] args) {
+		
+		int[] idades = new int[5];
+		String[] names = new String[5];
+		
+		int idade = 90;
+
+		Integer idadeRef = Integer.valueOf(idade);	//autoboxing
+		
+		System.out.println(idadeRef.doubleValue());
+		
+		int valor = idadeRef.intValue();	//unboxing
+		
+		String num = "100";
+		
+		Integer numero = Integer.valueOf(num);
+		int numParse = Integer.parseInt(num);
+		System.out.println(numParse);
+		
+		List<Integer> numeros = new ArrayList<Integer>();
+		numeros.add(100);	//AutoBoxing
+		
+		System.out.println("Valor máximo que um Integer pode guardar: " + Integer.MAX_VALUE);
+		System.out.println("Valor minímo que um Integer pode guardar: " + Integer.MIN_VALUE);
+		System.out.println("Tamanho do Integer em bits: " + Integer.SIZE);
+		System.out.println("Tamanho do Integer em bytes: " + Integer.BYTES);
+	}
+	
+
+}
